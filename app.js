2820ac28860e0c54d906930ce291c92e21633dc7
MarinaPrivalova <privalovama@yandex.ru>
2023-05-17 13:36:23 +0300

    feat: add express, nodemon, eslint, files, mongo, mongoose. add schemas and models for user and cards

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const app = express();
+
+mongoose.connect(MONGO_URL);
+
+app.listen(PORT);
